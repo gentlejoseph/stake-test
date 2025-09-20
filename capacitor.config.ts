20ffdcaa44c5105c9e6e46e69267d7ec7b3b98b4
~gentle <gentle.joseph@cashrewards.com>
2025-09-20 17:45:59 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stake.app',
+  appName: 'stake-ionic-app',
+  webDir: 'www'
+};
+
+export default config;
